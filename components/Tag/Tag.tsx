d0161a4b83e8560970ffa0f5b7f328aaf7789604
YuichiOta <rb19780519@gmail.com>
2023-06-08 10:13:33 +0900

    layout

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,9 +11,9 @@ const Tag = (props: Props) => {
     <div className="mx-4">
       <section className="lg:w-1/2 mb-8 mx-auto bg-gray-200 rounded-md p-5 shadow-2xl hover:shaodw-none hover:translate-y-1 duration-300 transition-all">
         <div className="font-medium mb-4">タグ検索</div>
-        <div className="grid grid-cols-3 gap-4 text-center">
+        <div className="h-50 grid grid-row-3 grid-flow-col gap-4 text-center">
           {tags.map((tag: string, index: number) => (
-            <span className="cursor-pointer px-2 font-medium pd-1 rounded-xl bg-green-200 inline-block" key={index}>
+            <span className="cursor-pointer font-medium pd-1 rounded-xl bg-green-200 inline-block" key={index}>
               <Link href={`/posts/tag/${tag}/page/1`}>
                 {tag}
               </Link>
