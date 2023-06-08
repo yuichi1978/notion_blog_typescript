317489641e7860f8943bf433c63cc2a0952c5a1c
YuichiOta <rb19780519@gmail.com>
2023-06-08 10:02:42 +0900

    layout修正

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,7 +11,7 @@ const Tag = (props: Props) => {
     <div className="mx-4">
       <section className="lg:w-1/2 mb-8 mx-auto bg-gray-200 rounded-md p-5 shadow-2xl hover:shaodw-none hover:translate-y-1 duration-300 transition-all">
         <div className="font-medium mb-4">タグ検索</div>
-        <div className="flex items-center justify-center">
+        <div className="grid grid-cols-3 gap-4 text-center">
           {tags.map((tag: string, index: number) => (
             <span className="cursor-pointer px-2 font-medium pd-1 rounded-xl bg-green-200 inline-block" key={index}>
               <Link href={`/posts/tag/${tag}/page/1`}>
