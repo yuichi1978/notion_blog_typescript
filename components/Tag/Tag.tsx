9eda48b7de01f8b62c8819c9a96e13c693bc8490
YuichiOta <rb19780519@gmail.com>
2023-06-08 13:37:14 +0900

    slug修正

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -11,7 +11,7 @@ const Tag = (props: Props) => {
     <div className="mx-4">
       <section className="lg:w-1/2 mb-8 mx-auto bg-gray-200 rounded-md p-5 shadow-2xl hover:shaodw-none hover:translate-y-1 duration-300 transition-all">
         <div className="font-medium mb-4 text-center">タグ検索</div>
-        <div className="flex justify-center flex-wrap">
+        <div className="flex place-content-evenly flex-wrap">
           {tags.map((tag: string, index: number) => (
             <span className="cursor-pointer mb-5 ml-2 p-2 font-medium w-2/5 md:w-1/4 rounded-xl bg-green-200 text-center" key={index}>
               <Link href={`/posts/tag/${tag}/page/1`}>
